8fd7a15aa47967e37069f8277d98824a98ee4180
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-10-07 23:11:02 -0300

    mudando bd

--- config/database.py
+++ config/database.py
@@ -4,8 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    raiz_dir = os.path.abspath(os.path.dirname(__file__))  # pasta config
-    db_path = os.path.join(raiz_dir, "../user.db")          # caminho relativo para raiz
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+    usuario = "root"          
+    senha = "nova_senha"       
+    host = "localhost"        
+    banco = "userdb"         
+    porta = 3306              
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f"mysql+pymysql://{usuario}:{senha}@{host}:{porta}/{banco}"
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
