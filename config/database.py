bde06cdfa41181f4ac270e9593d14b03b19e0f40
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-10-07 23:20:38 -0300

    run att

--- config/database.py
+++ config/database.py
@@ -4,15 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    usuario = "root"          
-    senha = "nova_senha"       
-    host = "localhost"        
-    banco = "userdb"         
-    porta = 3306              
+    # Pega as configurações do banco do ambiente, se disponíveis
+    usuario = os.getenv("DB_USER", "root")
+    senha = os.getenv("DB_PASS", "nova_senha")
+    host = os.getenv("DB_HOST", "localhost")
+    porta = int(os.getenv("DB_PORT", 3306))
+    banco = os.getenv("DB_NAME", "userdb")
 
+    # URI de conexão com PyMySQL
     app.config['SQLALCHEMY_DATABASE_URI'] = (
         f"mysql+pymysql://{usuario}:{senha}@{host}:{porta}/{banco}"
     )
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
