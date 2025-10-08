69a935cab8c5bdf4b5648b3d7abbe57861269cf3
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-10-07 23:35:49 -0300

    mudando bd

--- config/database.py
+++ config/database.py
@@ -4,17 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    # Pega as configurações do banco do ambiente, se disponíveis
-    usuario = os.getenv("DB_USER", "root")
-    senha = os.getenv("DB_PASS", "nova_senha")
-    host = os.getenv("DB_HOST", "localhost")
-    porta = int(os.getenv("DB_PORT", 3306))
-    banco = os.getenv("DB_NAME", "userdb")
+    usuario = os.getenv("DB_USER", "neondb_owner")
+    senha = os.getenv("DB_PASS", "npg_xCUHpsn1z0lY")
+    host = os.getenv("DB_HOST", "ep-cool-morning-ad79jm5x-pooler.c-2.us-east-1.aws.neon.tech")
+    porta = int(os.getenv("DB_PORT", 5432))
+    banco = os.getenv("DB_NAME", "neondb")
 
-    # URI de conexão com PyMySQL
     app.config['SQLALCHEMY_DATABASE_URI'] = (
-        f"mysql+pymysql://{usuario}:{senha}@{host}:{porta}/{banco}"
+        f"postgresql+psycopg2://{usuario}:{senha}@{host}:{porta}/{banco}?sslmode=require"
     )
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
